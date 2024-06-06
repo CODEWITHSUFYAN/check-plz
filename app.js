f734c8578da8c8cab11fe25b07adcfd18c9242d2
OWNER OF CODING SUFYAN <161226014+CODEWITHSUFYAN@users.noreply.github.com>
2024-06-06 05:44:32 -0700

    it is not for public

---
+++ app.js
@@ -0,0 +1,9 @@
+var email = prompt("Enter your email");
+var password = prompt("Enter your Password");
+
+
+if (email == sufiyanazeem5gmail.com || password == 123456 ) {
+    alert("Login Sucessfully"); 
+}else{
+    alert("plz try again");
+}
